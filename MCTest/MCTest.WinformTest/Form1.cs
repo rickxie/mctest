e5b2b516f1d773882d6637e2f29594356bfb5ad8
rickxiemc <rickxie@users.noreply.github.com>
2016-12-22 18:53:18 +0800

    all test

--- MCTest/MCTest.WinformTest/Form1.cs
+++ MCTest/MCTest.WinformTest/Form1.cs
@@ -9,10 +9,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin.Controls;
 
 namespace MCTest.WinformTest
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
